4d8dd4d1becb73c84717d521fc31242c567fdc17
hbrifat <hbrifat@gmail.com>
2025-01-01 22:49:25 +0600

    sort by price functionality done on both All pets & active categories

--- script/script.js
+++ script/script.js
@@ -67,12 +67,22 @@ const loadAllPets = () => {
     fetch("https://openapi.programming-hero.com/api/peddy/pets")
     .then((res) => res.json())
     // .then((data) => displayAllPets(data.pets))
-    .then((data) => loadSpinner(data.pets))
+    .then((data) => {
+      sortPricing(data.pets);
+
+      removeActiveClass();
+
+      loadSpinner(data.pets);
+
+    })
     .catch((error) => console.log(error));
 };
 
 
 const displayAllPets = (pets) => {
+        // // Sorting
+        // sortPricing(pets);
+
   // console.log(pets)
   const petShowcase = document.getElementById("pet-showcase");
 
@@ -182,7 +192,12 @@ const loadPetsByCategory = (category) => {
   fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
     .then((res) => res.json())
     // .then((data) => displayAllPets(data.data))
-    .then((data) => loadSpinner(data.data))
+    .then((data) => {
+      sortPricing(data.data);
+
+      loadSpinner(data.data);
+
+    })
     .catch((error) => console.log(error));
 };
 
@@ -250,19 +265,19 @@ const displayPetDetails = (petData) => {
     `;
 };
 
-// const myObj = {
-//   petId: 1,
-//   breed: "Golden Retriever",
-//   category: "Dog",
-//   date_of_birth: "2023-01-15",
-//   price: 1200,
-//   image: "https://i.ibb.co.com/p0w744T/pet-1.jpg",
-//   gender: "Male",
-//   pet_details:
-//     "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
-//   vaccinated_status: "Fully",
-//   pet_name: "Sunny",
-// };
+const sortPricing = (returnedValue) => {
+  document.getElementById("sorting-btn")
+      .addEventListener("click", function(){
+
+        returnedValue.sort(function(a, b){return b.price - a.price});
+
+        loadSpinner(returnedValue);
+        // console.log(returnedValue);
+  })
+}
+
+
+
 
 loadCategories();
 
